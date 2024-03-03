fe3348daa729b06c287729ed773c36d9f745cc5d
IvoJunior0 <shymanjr2019@gmail.com>
2024-03-03 16:28:57 -0300

    Versão 3

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const sol = document.querySelector('area[alt="Sol"]');
+const terra = document.querySelector('area[alt="Terra"]');
+const marte = document.querySelector('area[alt="Marte"]');
 let textoPlaneta = document.getElementById('texto-inf');
 
 sol.addEventListener("click", function(){
-    textoPlaneta.textContent = "Informações sobre o sol:";
+    textoPlaneta.textContent = "Informações sobre o Sol:";
+})
+terra.addEventListener("click", function(){
+    textoPlaneta.textContent = "Informações sobre a Terra:"
+})
+marte.addEventListener("click", function(){
+    textoPlaneta.textContent = "Informações sobre Marte:"
 })
